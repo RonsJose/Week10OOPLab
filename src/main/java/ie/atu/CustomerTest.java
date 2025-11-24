cd3ea9763d35fc0d028dba8496c5fde137dcad10
RonsJose <g00464215@atu.ie>
2025-11-24 09:55:04 +0000

    Created the customerTest class

---
+++ src/main/java/ie/atu/CustomerTest.java
@@ -0,0 +1,23 @@
+package ie.atu;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+        Customer part1Customer = new Customer(
+                "Alice Smith",
+                "1 High Street",
+                "0851234567",
+                1001,
+                true
+        );
+
+        Customer part2Customer = new Customer();
+        part2Customer.setName("Bob Jones");
+        part2Customer.setAddress("2 Low Street");
+        part2Customer.setPhoneNumber("0867654321");
+        part2Customer.setCustomerNumber(1002);
+        part2Customer.setMailingList(false);
+
+        System.out.println(part1Customer);
+        System.out.println(part2Customer);
+    }
+}
